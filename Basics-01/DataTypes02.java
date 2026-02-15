93743ae034521af19d0738e023b42c5c0b1c017f
unknown <asmtabhi@gmail.com>
2026-02-15 08:24:19 +0530

    1st Commit

---
+++ Basics-01/DataTypes02.java
@@ -0,0 +1,18 @@
+public class DataTypes02 {
+    public static void main(String[] args) {
+        //---Primitive data types------//
+
+        int num1=87;//Store an int value
+        float num2=89.985f;
+        double num3=6378.983759736;
+        char ch='a';
+
+
+        System.out.print(num1);
+        System.out.print(num2);
+        System.out.print(num3);
+        System.out.print(ch);
+
+
+    }
+}
