e416d3119bdae791d3301b8ab3af80cdb5161eda
unknown <asmtabhi@gmail.com>
2026-02-17 21:31:39 +0530

    Final Commit

--- 03-Integers/Fibbonacci.java
+++ 03-Integers/Fibbonacci.java
@@ -1,22 +1,36 @@
 import java.util.*;
+
 public class Fibbonacci {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number : ");
         int x =in.nextInt();
 
 
         int a=0;
         int b=1;
-        int sum =1;
+
+
+        // int sum =1;
         
-        System.out.print(a+" "+b+" "+sum+" ");
-        a++;
+        // System.out.print(a+" "+b+" "+sum+" ");
+        // a++;
+
+        // for(int i=2;i<x;i++){
+        //     sum=a+b;
+        //     System.out.print(sum+" ");
+        //     a=b;
+        //     b=sum;
+        // }
+
         
-        for(int i=2;i<=x;i++){
-            sum=a+b;
-            System.out.print(sum+" ");
+        System.out.print("Fibonacci series : ");
+
+        while (a<=x){
+            System.out.print(a+" ");
+            int sum = a+b;
             a=b;
             b=sum;
-        }
+        } 
     }    
 }
