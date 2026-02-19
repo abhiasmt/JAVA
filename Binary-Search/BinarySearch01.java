c52129d79f6e32369c8ebdc2965aec67c65e3ffa
unknown <asmtabhi@gmail.com>
2026-02-19 18:23:31 +0530

    1st Commit

---
+++ Binary-Search/BinarySearch01.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+public class BinarySearch01 {
+    public static void BinarySearch(int[] arr, int target){
+        int start=0;
+        int end=arr.length-1;
+        boolean found=false;
+        int mid=0;
+
+        while (start<=end) {
+            mid=(start+end)/2;
+
+            if (arr[mid]==target) {
+                found=true;
+                break;
+            }else if(arr[mid]>target){
+                end=mid-1;
+            }else if(arr[mid]<target){
+                start=mid+1;
+            }
+        }
+        if (found==true) {
+            System.out.print("Element found at : "+mid);
+        }else{
+            System.out.print("Element not found");   
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        System.out.print("Enter the array size : ");
+        int sz=in.nextInt();
+
+        int[] arr=new int[sz];
+
+        System.out.print("Enter the array : ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=in.nextInt();
+        }
+
+        System.out.print("Enter the target : ");
+        int target=in.nextInt();
+
+        BinarySearch(arr, target);
+    }
+}
