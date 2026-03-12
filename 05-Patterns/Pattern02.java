146937e433067622970c5390afe6fe04f0aa08f4
unknown <asmtabhi@gmail.com>
2026-03-12 21:08:11 +0530

    first

---
+++ 05-Patterns/Pattern02.java
@@ -0,0 +1,18 @@
+
+
+public class Pattern02 {
+    public static void main(String[] args) {
+        Patrn02(5); 
+    }
+
+    public static void Patrn02(int n){
+        int col=1;
+        for(int row=1;row<=n;row++){
+            while (col==0) {
+                if (row!=n) {
+                    
+                }
+            }
+        }
+    }
+}
